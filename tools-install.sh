c5aa493c6fca3a7d05dc6b26f8d2340643c8a010
Qbing <38577566+qb1ng@users.noreply.github.com>
2020-04-02 22:11:07 -0400

    Create tools-install.sh
    Including python3, pwntools for python3, gdb, gdb-peda

---
+++ tools-install.sh
@@ -0,0 +1,17 @@
+# update
+sudo apt-get update
+
+# install python3
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+
+# install pwntools
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade git+https://github.com/Gallopsled/pwntools.git@dev
+
+# install gdb
+sudo apt-get install gdb
+
+# install gdb-peda
+sudo git clone https://github.com/longld/peda.git ~/peda
+sudo echo "source ~/peda/peda.py" >> ~/.gdbinit
+
